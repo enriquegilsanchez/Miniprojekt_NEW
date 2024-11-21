8440a7c08fd3977b29c14783fb5700f49ed06545
Enrique <enrique.gil-sanchez@tu-dortmunde.de>
2024-11-21 16:58:49 +0100

    tweaked generation

--- Assets/Scripts/GraphScripts/GraphGenerator.cs
+++ Assets/Scripts/GraphScripts/GraphGenerator.cs
@@ -11,15 +11,13 @@ public class GraphGenerator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Rect rect = new Rect(960, 540, 300, 300);
+        Rect rect = new Rect(960, 540, 50, 50);
         tree = BspTree.splitTree(4, rect);
     }
     void OnDrawGizmos()
     {
         if (DebugDraw && tree != null)
         {
-            // Gizmos.color = Color.red;
-            // Gizmos.DrawCube(new Vector3(960, 540, 0), new Vector3(300, 300, 0));
             DrawBspTree(tree);
         }
     }
